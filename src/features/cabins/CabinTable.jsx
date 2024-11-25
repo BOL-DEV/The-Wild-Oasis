238752a2f5f56c32f04ff4dd77c71218226d8a34
BOL-DEV <abdulakeemhabeeb123@gmail.com>
2024-11-25 13:13:08 +0100

    Adding filters

--- src/features/cabins/CabinTable.jsx
+++ src/features/cabins/CabinTable.jsx
@@ -1,3 +1,4 @@
+import { useSearchParams } from "react-router-dom";
 import Menus from "../../ui/Menus";
 import Spinner from "../../ui/Spinner";
 import Table from "../../ui/Table";
@@ -6,6 +7,20 @@ import useCabins from "./useCabins";
 
 function CabinTable() {
   const { isLoading, cabins } = useCabins();
+
+  const [searchParams] = useSearchParams();
+
+  const filterValue = searchParams.get("discount") || "all";
+
+  let filterdCabins;
+
+  filterValue === "with-discount"
+    ? (filterdCabins = cabins.filter((cabin) => cabin.discount > 0))
+    : filterValue === "no-discount"
+    ? (filterdCabins = cabins.filter((cabin) => cabin.discount === 0))
+    : filterValue === "all"
+    ? (filterdCabins = cabins)
+    : (filterdCabins = []);
 
   if (isLoading) return <Spinner />;
 
@@ -22,7 +37,8 @@ function CabinTable() {
         </Table.Header>
 
         <Table.Body
-          data={cabins}
+          // data={cabins}
+          data={filterdCabins}
           render={(cabin) => <CabinRow cabin={cabin} key={cabin.id} />}
         />
 
